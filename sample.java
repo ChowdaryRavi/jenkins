645abcd6166d8fc0d3750f40161f118b0eff4dc6
ChowdaryRavi <116291847+ChowdaryRavi@users.noreply.github.com>
2024-01-04 15:42:07 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,7 @@
 public class sample {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("***************");
+        System.out.println("Hello! from GitHUB");
+        System.out.println("***************");
     }
 }
